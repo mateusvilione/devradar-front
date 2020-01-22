0538d7090c45518a1b808ae6b16adee248c4c67b
Mateus Vilione <mateusvilione@gmail.com>
2020-01-22 20:09:39 -0300

    isolando o componente de listagem de devs

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import './Sidebar.css'
 import './Main.css';
 
+import DevItem from './components/DevItem';
 
 function App() {
   const [ devs, setDevs ] = useState([]);
@@ -99,17 +100,7 @@ function App() {
       <main>
         <ul>
           {devs.map(dev => (
-            <li key={dev._id} className="dev-item">
-              <header>
-                <img src={dev.avatar_url} alt={dev.name}/>
-                <div className="user-info">
-                  <strong>{dev.name}</strong>
-                  <span>{dev.techs.join(', ')}</span>
-                </div>
-              </header>
-              <p>{dev.bio}</p>
-              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
-            </li>
+            <DevItem key={dev._id} dev={dev}/>
           ))}
         </ul>
       </main>
